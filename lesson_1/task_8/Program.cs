7c202ffa7b61bf521a973d698c455c5ae94ce192
vasilakiy <slk55slk@gmail.com>
2022-07-25 01:30:38 +0300

    task 8

---
+++ lesson_1/task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int numberA = 0;
+
+Console.Write("Введите число ");
+numberA = int.Parse(Console.ReadLine());
+
+for (int x = 2; x <= numberA; x++)
+  if (x % 2 == 0)
+    Console.Write($"{x} ");
