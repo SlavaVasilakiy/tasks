ecc95b24fc977ff78d9823fe50c2ca6916440e46
vasilakiy <slk55slk@gmail.com>
2022-07-25 01:20:38 +0300

    task 6

---
+++ lesson_1/task_6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int numberA = 0;
+
+Console.Write("Введите число ");
+numberA = int.Parse(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+  Console.Write($"Число {numberA} четное");
+}
+else
+{
+  Console.Write($"Число {numberA} нечетное");
+}
