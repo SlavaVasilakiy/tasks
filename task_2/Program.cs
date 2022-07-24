c4f3f2a5cbb9add9d32afafdf8fbaeabcbf8e237
vasilakiy <slk55slk@gmail.com>
2022-07-25 00:50:23 +0300

    task 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int numberA = 0;
+int numberB = 0;
+//int max = 0;
+//int min = 0;
+
+Console.Write("Введите первое число ");
+numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+
+{
+  // max = numberA;
+  // min = numberB;
+  Console.Write($"{numberA} больше, чем {numberB}");
+}
+else
+{
+  //max = numberB;
+  //min = numberA;
+  Console.Write($"{numberB} больше, чем {numberA}");
+}
+
