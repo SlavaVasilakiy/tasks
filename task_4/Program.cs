187cc58f93969af3b55e71d4a130aad53f9c210b
vasilakiy <slk55slk@gmail.com>
2022-07-25 01:05:25 +0300

    task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int numberA = 0;
+int numberB = 0;
+int numberC = 0;
+int max = numberA;
+
+Console.Write("Введите первое число ");
+numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+numberB = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+numberC = int.Parse(Console.ReadLine());
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write($"Максимальное число {max}");
