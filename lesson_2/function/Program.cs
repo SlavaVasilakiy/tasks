d2e9b12c91766de05f2b832fa716913f6a9d6a27
vasilakiy <slk55slk@gmail.com>
2022-07-25 17:23:20 +0300

    function

---
+++ lesson_2/function/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int a = 0;
+a = int.Parse(Console.ReadLine());
+
+
+void Sqr(int temp, int temp2, double temp3)
+{
+  temp2 = int.Parse(Console.ReadLine());
+  Console.WriteLine($"Sqr A it's {temp * temp}");
+}
+
+Sqr(a, 5, 6);
