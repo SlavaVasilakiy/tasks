4f6a0e8b5a79050f510d043dc168b402793b30ac
vasilakiy <slk55slk@gmail.com>
2022-07-26 12:09:07 +0300

    lesson3

---
+++ lesson_3/random/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+  Console.Write("да");
+}
+else
+{
+  Console.Write("нет");
+}
