59949cb35c2d2786a0132f69ac07c8518b8fa68c
vasilakiy <slk55slk@gmail.com>
2022-08-09 13:13:15 +0300

    Lesson 9

---
+++ lesson_9/task_66/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1;
+N = 15-> 120
+M = 4;
+N = 8. -> 30*/
+
+Console.Write("Введите начальную цифру: ");
+int m = 0;
+int.TryParse(Console.ReadLine(), out m);
+
+Console.Write("Введите конечную цифру: ");
+int n = 0;
+int.TryParse(Console.ReadLine(), out n);
+
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+string PrintNumbers(int m, int n)
+{
+    if (m == n) 
+        return m.ToString();
+     return (m + " " + PrintNumbers(m + 1, n));
+}
+
+Console.Write(PrintNumbers(m, n));
+Console.WriteLine();
+
+void SumNumbers(int m, int n, int sum)
+    {
+    sum += n;
+        if (n <= m)
+        {
+        Console.Write($"Сумма = {sum}");
+        return;
+        }
+        SumNumbers(m, n - 1, sum);
+    }
+
+SumNumbers(m,n,temp=0);
+
+Console.ReadKey();
---
+++ lesson_9/task_68/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3->A(m, n) = 29 
+
+
+ОПЯТЬ НЕ ПРАВИЛЬНЫЙ ОТВЕТ В ЗАДАЧЕ !!! СНОВА ГОЛОВУ ЛОМАЛ ЧТО НЕ ТАК С МОИМ КОДОМ )))*/
+
+Console.Write("Введите m: ");
+int m = 0;
+int.TryParse(Console.ReadLine(), out m);
+
+Console.Write("Введите n: ");
+int n = 0;
+int.TryParse(Console.ReadLine(), out n);
+
+int function = A(m, n);
+
+int A(int m, int n)
+{
+  if (m == 0)
+        {
+                return n + 1;
+        } 
+  else if (n == 0)
+        {
+                return A(m - 1, 1);
+        }
+  else
+        {
+                return A(m - 1, A(m, n - 1));
+        }
+
+}
+
+Console.Write($"A(m, n) = {function} ");
+
+Console.ReadKey();
